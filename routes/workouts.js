e039ec0aa8faafccc68c8d5e48fc43cb920566fd
lcrocket <liamcrocket@gmail.com>
2020-06-06 17:48:27 -0400

    Added get, update, and delete endpoints for workouts

--- routes/workouts.js
+++ routes/workouts.js
@@ -7,6 +7,12 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Workout.findById(req.params.id)
+    .then((workout) => res.json(workout))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 router.route("/add").post((req, res) => {
   const name = req.body.name;
   const description = req.body.description;
@@ -21,6 +27,27 @@ router.route("/add").post((req, res) => {
   newWorkout
     .save()
     .then(() => res.json("Workout added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").put((req, res) => {
+  Workout.findById(req.params.id)
+    .then((workout) => {
+      workout.name = req.body.name;
+      workout.description = req.body.description;
+      workout.date = req.body.date;
+
+      workout
+        .save()
+        .then(() => res.json("Workout updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Workout.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Workout deleted."))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
