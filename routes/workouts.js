fb5e2ff19749f19c7e4d6d7b5e8b4aedfb160dda
lcrocket <liamcrocket@gmail.com>
2020-06-14 18:39:50 -0400

    Added endpoint to get workouts by user

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.model");
 
-router.route("/").get((req, res) => {
-  Workout.find()
-    .then((workouts) => res.json(workouts))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
 router.route("/:id").get((req, res) => {
   Workout.findById(req.params.id)
     .then((workout) => res.json(workout))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/user/:id").get((req, res) => {
+  Workout.find({ user: req.body.userId })
+    .then((workouts) => res.json(workouts.sort((a, b) => b.date - a.date)))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
