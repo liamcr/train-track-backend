0f0999b862fbdef4fc681e4e7601371fcf157d65
lcrocket <liamcrocket@gmail.com>
2020-06-27 18:10:55 -0400

    Added unfollow endpoint

--- routes/users.js
+++ routes/users.js
@@ -43,12 +43,16 @@ router.route("/follow/:id").post(authenticateJWT, async (req, res) => {
       User.findById(req.params.id)
         .then((userToFollow) => {
           // If a user if trying to follow someone they are already following,
-          // response with "Bad Request"
+          // respond with "Bad Request"
           if (
             currentUser.following.findIndex(userToFollow.id) !== -1 ||
             userToFollow.followers.findIndex(currentUser.id) !== -1
           ) {
-            res.status(400).json("Error: User is already following");
+            res
+              .status(400)
+              .json(
+                "Error: User is attempting to follow someone they already follow"
+              );
           }
 
           currentUser.following.push(userToFollow.id);
@@ -61,6 +65,56 @@ router.route("/follow/:id").post(authenticateJWT, async (req, res) => {
                 .save()
                 .then(() => {
                   res.json("Followed!");
+                })
+                .catch((err) => res.status(400).json("Error: " + err))
+            )
+            .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) =>
+          res
+            .status(404)
+            .json(`Error: Could not find user with id ${req.params.id}`)
+        );
+    })
+    .catch((err) =>
+      res
+        .status(404)
+        .json(`Error: Could not find user with id ${req.user.userId}`)
+    );
+});
+
+router.route("/unfollow/:id").post(authenticateJWT, async (req, res) => {
+  User.findById(req.user.userId)
+    .then((currentUser) => {
+      User.findById(req.params.id)
+        .then((userToUnfollow) => {
+          // If a user if trying to unfollow someone they aren't already following,
+          // respond with "Bad Request"
+          if (
+            currentUser.following.findIndex(userToUnfollow.id) === -1 ||
+            userToUnfollow.followers.findIndex(currentUser.id) === -1
+          ) {
+            res
+              .status(400)
+              .json(
+                "Error: User is attempting to unfollow someone they don't yet follow"
+              );
+          }
+
+          currentUser.following = currentUser.following.filter(
+            (userFollowed) => userFollowed !== userToUnfollow.id
+          );
+          userToUnfollow.followers = userToUnfollow.followers.filter(
+            (follower) => follower !== currentUser.id
+          );
+
+          currentUser
+            .save()
+            .then(() =>
+              userToUnfollow
+                .save()
+                .then(() => {
+                  res.json("Unfollowed!");
                 })
                 .catch((err) => res.status(400).json("Error: " + err))
             )
