02cf9da5d4c5350f4e2ef9fb7b32ffbf84c260c0
lcrocket <liamcrocket@gmail.com>
2020-07-04 11:59:44 -0400

    Added timeline endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 const User = require("../models/user.model");
+const Workout = require("../models/workout.model");
 const authenticateJWT = require("../middleware/authenticate");
 
 router.route("/register").post((req, res) => {
@@ -143,6 +144,39 @@ router.route("/unfollow/:id").post(authenticateJWT, async (req, res) => {
         .status(404)
         .json(`Error: Could not find user with id ${req.user.userId}`)
     );
+});
+
+router.route("/timeline").get(authenticateJWT, (req, res) => {
+  const limit = req.body.limit ? req.body.limit : 10;
+  const offset = req.body.offset ? req.body.offset : 0;
+
+  if (!Number.isInteger(limit) || !Number.isInteger(offset)) {
+    res.status(400).json("Error: limit and offset params must be integers");
+  }
+
+  User.findById(req.user.userId)
+    .then((currentUser) => {
+      Workout.find({
+        user: { $in: currentUser.following },
+      })
+        .then((workouts) => {
+          let timeline = workouts
+            .sort((a, b) => b.date - a.date)
+            .slice(offset, offset + limit);
+
+          res.json(timeline);
+        })
+        .catch((err) => {
+          res
+            .status(400)
+            .json(`Error: Something went wrong finding workouts - ${err}`);
+        });
+    })
+    .catch((err) => {
+      res
+        .status(404)
+        .json(`Error: Could not find user with id ${req.user.userId}`);
+    });
 });
 
 module.exports = router;
