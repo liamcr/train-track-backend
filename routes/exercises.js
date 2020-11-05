458283fe2e19d1a4a9b60f46dfe87cf21f2d38dc
lcrocket <liamcrocket@gmail.com>
2020-11-05 12:11:10 -0500

    Added /addBulk exercise endpoint

--- routes/exercises.js
+++ routes/exercises.js
@@ -50,6 +50,48 @@ router.route("/add").post(authenticateJWT, (req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/addBulk").post(authenticateJWT, async (req, res) => {
+  const workoutId = req.body.workoutId;
+  const exercises = req.body.exercises;
+
+  for (let i = 0; i < exercises.length; i++) {
+    let newExercise = new Exercise({
+      user: req.user.userId,
+      name: exercises[i].name,
+      description: exercises[i].description,
+      sets: exercises[i].sets,
+    });
+
+    await newExercise
+      .save()
+      .then(async () => {
+        await Workout.findById(workoutId)
+          .then(async (workout) => {
+            if (workout.user.toString() !== req.user.userId) {
+              return res.sendStatus(403);
+            }
+
+            if (workout.exerciseIds.includes(newExercise.id)) {
+              return res
+                .status(400)
+                .json(
+                  "Error: User is attempting to add an exercise that is already in the workout"
+                );
+            }
+            workout.exerciseIds.push(newExercise.id);
+
+            await workout
+              .save()
+              .catch((err) => res.status(400).json("Error: " + err));
+          })
+          .catch((err) => res.status(404).json("Error: " + err));
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  }
+
+  res.json("Exercises Added");
+});
+
 router.route("/update/:id").put(authenticateJWT, (req, res) => {
   Exercise.findById(req.params.id)
     .then((exercise) => {
