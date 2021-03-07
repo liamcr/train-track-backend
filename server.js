c98f81636ce4b46d3eaa5df9c6a11edf62167824
lcrocket <liamcrocket@gmail.com>
2021-03-07 12:15:18 -0500

    Updated dockerfiles to enable hot reloading

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
